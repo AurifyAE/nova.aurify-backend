520e8b34d8a7aedfddc4cda09467bf98a4c6199d
ABDUL MAJID K <majidmaji3410@gmail.com>
2024-09-04 20:11:28 +0530

    device:macAddress bug issue fixed

--- middleware/deviceManage.js
+++ middleware/deviceManage.js
@@ -21,7 +21,6 @@ export const deviceManagementMiddleware = async (req, res, next) => {
 
     const activeScreens = deviceDoc.devices.filter((d) => d.isActive).length;
 
-    // Check if the number of active screens is greater than or equal to the limit
     if (activeScreens > admin.screenLimit) {
       return res.status(403).json({
         message: `Screen limit exceeded. You are allowed a maximum of ${admin.screenLimit} active screen(s). Currently, there are ${activeScreens} active screen(s).`,
@@ -32,6 +31,8 @@ export const deviceManagementMiddleware = async (req, res, next) => {
     next();
   } catch (error) {
     console.error("Middleware error:", error);
-    return res.status(500).json({ message: "Internal server error", error: error.message });
+    return res
+      .status(500)
+      .json({ message: "Internal server error", error: error.message });
   }
 };
