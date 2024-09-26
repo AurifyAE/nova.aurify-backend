dd8ab102f040c29467b2692440cb8f441138a434
ABDUL MAJID K <majidmaji3410@gmail.com>
2024-09-26 15:07:50 +0530

    chnage the db connection

--- config/connaction.js
+++ config/connaction.js
@@ -1,17 +1,15 @@
 import mongoose from "mongoose";
 mongoose.set('strictQuery', false)
-const mongodb = async ()=>{
+const mongodb = async () => {
     try {
-        await mongoose.connect(`mongodb+srv://tecnaviswebsolutions:${process.env.MONGOOES_PASS}@cluster0.5ycuo.mongodb.net/Demo-Aurify?retryWrites=true&w=majority&appName=Cluster0`).then(()=>{
+        await mongoose.connect(`mongodb+srv://tecnaviswebsolutions:${process.env.MONGOOES_PASS}@aurify.tleb8.mongodb.net/Aurify-Database?retryWrites=true&w=majority&appName=Aurify`).then(() => {
             console.log("connection successful");
-        }).catch((error)=>{
-            console.log("no connected");
+        }).catch((error) => {
             console.log(error);
         })
     } catch (error) {
         console.log(error);
-        
+
     }
 }
-export  {mongodb}
-
+export {mongodb}
