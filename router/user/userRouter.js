bc0e2c2c89c89eb23b65c60a4c84704ec40df776
Rameesha <rameeshasharin05@gmail.com>
2024-10-08 16:01:40 +0530

    added premium discount to user

--- router/user/userRouter.js
+++ router/user/userRouter.js
@@ -1,27 +1,28 @@
 import { Router } from "express";
 import {
-  registerUser,
-  updateSpread,
-  userLoginController,
-  requestAdmin,
-  getServerDetails,
-  getCommodities,
-  getCurrentNews,
-  getSpotrateDetails,
-  forgotPassword,
-  fetchAdminBankDetails
-} from "../../controllers/user/userController.js";
-import {
-  getProductDetails,
   addItemToCart,
-  incrementCartItem,
+  addItemToWishlist,
   decrementCartItem,
   deleteCartItem,
+  deleteWishlist,
+  getProductDetails,
   getUserCart,
-  addItemToWishlist,
   getUserWishlist,
-  deleteWishlist
+  incrementCartItem,
 } from "../../controllers/user/productController.js";
+import {
+  fetchAdminBankDetails,
+  forgotPassword,
+  getCommodities,
+  getCurrentNews,
+  getPremiumDiscounts,
+  getServerDetails,
+  getSpotrateDetails,
+  registerUser,
+  requestAdmin,
+  updateSpread,
+  userLoginController,
+} from "../../controllers/user/userController.js";
 
 const router = Router();
 router.post("/register/:adminId", registerUser);
@@ -29,7 +30,7 @@ router.post("/login/:adminId", userLoginController);
 router.patch("/update-spread/:adminId/:userId", updateSpread);
 router.post("/request-admin/:adminId", requestAdmin);
 router.get("/get-spotrates/:adminId", getSpotrateDetails);
-router.get("/get-banks/:adminId",fetchAdminBankDetails)
+router.get("/get-banks/:adminId", fetchAdminBankDetails);
 router.get("/get-news/:adminId", getCurrentNews);
 router.get("/get-commodities/:adminId", getCommodities);
 router.get("/get-server", getServerDetails);
@@ -43,5 +44,6 @@ router.delete("/cart/:adminId/:userId/:productId", deleteCartItem);
 router.patch("/wishlist/:adminId/:userId/:productId", addItemToWishlist);
 router.delete("/wishlist/:adminId/:userId/:productId", deleteWishlist);
 router.put("/forgot-password/:adminId/:userId", forgotPassword);
+router.get("/get-premium-discount/:adminId", getPremiumDiscounts); // New route for premium discounts
 
 export default router;
