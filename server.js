a6e44955589952db6a933c05891cb4d5d8a8c9ab
ABDUL MAJID K <majidmaji3410@gmail.com>
2025-02-26 20:03:29 +0530

    first commit

--- server.js
+++ server.js
@@ -3,19 +3,14 @@ import cors from "cors";
 import dotenv from "dotenv";
 import adminRouter from './router/admin/adminRouter.js'
 import superRouter from './router/super/superRouter.js'
-import deviceRouter from './router/device/deviceRouter.js'
 import userRouter from './router/user/userRouter.js'
 import { mongodb } from "./config/connaction.js";
 import { errorHandler } from "./utils/errorHandler.js";
-import { io } from "./config/socket_io.js";
-import socketApi from "./utils/socket_API.js"
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 4444;
-
-
 
 app.use(express.static('public'));
 app.use(express.json({ limit: '50mb' })); // Increase JSON payload limit
@@ -50,18 +45,12 @@ mongodb();
 // Routes
 app.use("/api", adminRouter);
 app.use("/admin", superRouter);
-app.use("/device", deviceRouter);
 app.use("/user", userRouter);
 
 // Global error handling middleware
 app.use(errorHandler);
 
-
-const server = app.listen(port, () => {
+app.listen(port, () => {
   console.log("server running !!!!!");
   console.log(`http://localhost:${port}`);
 });
-
-
-io.attach(server)
-socketApi()
