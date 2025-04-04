398a780089497ebfc413b7e47fe5d6d0b3e92f42
ABDUL MAJID K <majidmaji3410@gmail.com>
2025-04-04 18:12:14 +0530

    update the name & remove the pushnotifiactionjson file

--- utils/firebase.js
+++ utils/firebase.js
@@ -1,5 +1,4 @@
 import admin  from "firebase-admin"
-import serviceAccount from "./pushnotifaction.json" assert { type: "json" };
 import dotenv from "dotenv";
 dotenv.config();
 const credentials = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS);
